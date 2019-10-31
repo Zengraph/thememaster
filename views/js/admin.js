f0e3302dbf8fc1c664c795fd222d8af246bf69eb
Zengraph <nicozengraph@gmail.com>
2019-10-31 22:47:47 +0100

    Update admin.js

--- views/js/admin.js
+++ views/js/admin.js
@@ -1,11 +1,11 @@
 jQuery(function($){
-	$('#thememaster .panel').hide(); 
-	$('#configuration_fieldset_0').show();
-	$('.nav-tabs li:first').addClass('active');
-	$('.nav-tabs a').click(function(){
+    $('#thememaster .panel').hide(); 
+    $('#configuration_fieldset_0').show();
+    $('.nav-tabs li:first').addClass('active');
+    $('.nav-tabs a').click(function(){
         $(this).parent().addClass('active').siblings().removeClass('active');
         var fieldsetID = $(this).attr('data-fieldset').split(',');	
-		$('#thememaster .panel').hide();
+	$('#thememaster .panel').hide();
         $.each(fieldsetID,function(i,n){
             $('#configuration_fieldset_' + n).show();
         });
